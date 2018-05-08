812799ffdd1ff3659ca726e0d5f14125065389db
S Rajitha <rajitha1911@gmail.com>
2018-05-08 19:21:47 +0530

    created weather object

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ searchBtn.addEventListener("click", searchWeatherData);
 function searchWeatherData() {
     var userInput = cityName.value;
 
-    if(userInput.trim() == 0){
+    if (userInput.trim() == 0) {
         alert("Enter City Name");
-    }else {
+    } else {
         var http = new XMLHttpRequest();
         var apiKey = "546f6e6e2e9ada0d51dab595a78f5c97";
         var url = "http://api.openweathermap.org/data/2.5/weather?q=" + userInput;
@@ -15,9 +15,9 @@ function searchWeatherData() {
 
         http.open(method, url);
         http.onreadystatechange = function () {
-            if(http.readyState === XMLHttpRequest.DONE && http.status === 200){
-
-            }else if(http.readyState === XMLHttpRequest.DONE){
+            if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
+                var data = JSON.parse(http.responseText);
+            } else if (http.readyState === XMLHttpRequest.DONE) {
                 alert("Something Went Wrong !");
             }
         };
