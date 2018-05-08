79c22c7460fe7c1077acef403d394a6479cef1f6
S Rajitha <rajitha1911@gmail.com>
2018-05-08 20:02:13 +0530

    finished project

--- init.js
+++ init.js
@@ -2,3 +2,9 @@
 
 var searchBtn = document.querySelector("button");
 var cityName = document.querySelector("#city");
+
+var loadingText = document.querySelector("#load");
+var weatherDiv = document.querySelector("#weather");
+var weatherCity = document.querySelector("#weatherCity");
+var weatherDescription = document.querySelector("#weatherDescription");
+var weatherTemperature = document.querySelector("#weatherTemperature");
--- script.js
+++ script.js
@@ -3,6 +3,10 @@
 searchBtn.addEventListener("click", searchWeatherData);
 
 function searchWeatherData() {
+
+    loadingText.style.display = "block";
+    weatherDiv.style.display = "none";
+
     var userInput = cityName.value;
 
     if (userInput.trim() == 0) {
@@ -10,16 +14,32 @@ function searchWeatherData() {
     } else {
         var http = new XMLHttpRequest();
         var apiKey = "546f6e6e2e9ada0d51dab595a78f5c97";
-        var url = "http://api.openweathermap.org/data/2.5/weather?q=" + userInput;
+        var url = "http://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&units=metric&appid="+apiKey;
         var method = "GET";
 
         http.open(method, url);
         http.onreadystatechange = function () {
             if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
+
                 var data = JSON.parse(http.responseText);
+                var weatherData = new Weather(userInput, data.weather[0].description.toUpperCase());
+                weatherData.temperature = data.main.temp;
+
+                updateWeather(weatherData);
             } else if (http.readyState === XMLHttpRequest.DONE) {
                 alert("Something Went Wrong !");
             }
         };
+        http.send();
     }
+}
+
+
+function updateWeather(weatherData) {
+    weatherCity.textContent = weatherData.cityName;
+    weatherDescription.textContent = weatherData.description;
+    weatherTemperature.textContent = weatherData.temperature;
+
+    loadingText.style.display = "none";
+    weatherDiv.style.display = "block";
 }
